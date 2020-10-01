2bc37577aa0affd7f3f02bc132c1b62aed7f0234
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-10-01 16:39:45 +0200

    Add files via upload

---
+++ lec_2_for.py
@@ -0,0 +1,14 @@
+#for i in 1,2,3,4:
+#    print(i**2, end=' ')
+#    
+#for i in 1,2,3,4:
+#    print(i**2, sep=' ')   
+
+#a=[1,2,3,4,5]
+#for i in a:
+#    print(i**3, end=' "Yes" ')
+
+#for i in range(0,10,1):
+#    print(i, end=' ')
+  
+    
